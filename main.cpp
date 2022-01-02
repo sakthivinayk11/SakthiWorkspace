e427773243057060b745955430b7316d434365ad
sakthi <sakthi.11vinayk@gmail.com>
2022-01-02 16:35:55 +0530

    Add the class to the main

--- main.cpp
+++ main.cpp
@@ -4,6 +4,27 @@
 
 using namespace std;
 
+class Temp{
+    private:
+    int a;
+    public:
+    Temp(int aa):a{aa}{
+        cout<<"ParamConst "<<this->a<<endl;
+    }
+    void printfunc(void);
+    void callUsingPrint(void);
+    using FunPtr = void(Temp::*)();
+    FunPtr newFun = &Temp::callUsingPrint;
+};
+
+void Temp::printfunc(void){
+    std::cout<<"PrintFun"<<endl;
+}
+
+void Temp::callUsingPrint(void){
+    std::cout<<"callUsingPrint"<<endl;
+}
+
 void fun(void){
     cout<<"Function Pointer"<<endl;
 }
@@ -15,9 +36,15 @@ int main()
     {
         cout << word << " ";
     }
+    cout << endl;
 
     using FunPtr = void(*)(void);
     FunPtr FunPtradd = &fun;
     FunPtradd();
-    cout << endl;
+
+    Temp objForTemp(20);
+    Temp *ptr = &objForTemp;
+    ptr->printfunc();
+    //(ptr->*newFun)();
+   // (objForTemp.Temp::newFun)();
 }
